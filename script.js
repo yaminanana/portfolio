ba9829024be691822c49f65c40d28de5fb5f8d4b
yamina <amouryamina3@gmail.com>
2021-01-30 10:05:14 +0100

    commit initial

---
+++ script.js
@@ -0,0 +1,31 @@
+window.onscroll = function (){
+    if(document.documentElement.scrollTop > 300){
+        document.getElementById("navbar").style.backgroundColor = "#636580";
+        document.querySelector("li a ").style.color = "white";
+        document.getElementById("navbar").style.padding = "30px 10px";
+
+    }else{
+        document.getElementById("navbar").style.backgroundColor = "#a9abbc";
+        document.querySelector("a").style.color = "black";
+        document.getElementById("navbar").style.padding = "15px 10px";
+    };
+
+
+};
+
+function sendMail(params){
+let tempParams = {
+    from_name:document.getElementById('mail').Value,
+    to_name:document.getElementById('name').Value,
+    message:document.getElementById('msg').Value,
+};
+emailjs.send('service_4qcxtws','template_yq3jb9r',tempParams )
+.then(function(res){
+    console.log("success",res.status);
+})
+}
+
+
+
+
+     
