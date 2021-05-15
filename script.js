ffd76a3829ea894adb9c2b734207916ec617b474
amouryamina3 <amouryamina3@gmail.com>
2021-05-16 00:08:14 +0200

    Ajoute le menu responsive

--- script.js
+++ script.js
@@ -43,4 +43,11 @@ window.onload = function() {
 }
 
 
-     
+const burgerButton = document.getElementsByClassName('burger-button')[0];
+const lienmenu = document.getElementsByClassName('lienmenu')[0];
+
+
+burgerButton.addEventListener('click',() => {
+lienmenu.classList.toggle('active')
+
+})   
